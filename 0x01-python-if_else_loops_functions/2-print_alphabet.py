dd18877bea57fb707c752b78fca9b7e8ff1b30c7
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-03 21:03:52 +0000

    rewriting print format slightly to salve the fury of the checker

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for ord_num in range(97, 123):
-    print(chr(ord_num), end="")
+    print("{}".format(chr(ord_num)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ord_num in range(97, 123):
     if ord_num != 101 and ord_num != 113:
-        print(chr(ord_num), end="")
+        print("{}".format(chr(ord_num)), end="")
