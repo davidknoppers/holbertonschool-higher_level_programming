9ee4e2f0f2dbb2f4dcd7035da8920f86849aa21a
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-06 00:10:07 +0000

    put var declaration within if statement

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-a, b = 10, 5
 if __name__ == "__main__":
+    a = 10
+    b = 5
     from calculator_1 import add, sub, mul, div
     math_fns = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
     math_symbols = ['+', '-', '*', '/']
