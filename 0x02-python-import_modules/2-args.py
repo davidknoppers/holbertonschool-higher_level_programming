d0b91f00701cc995ead4796ed30aec30acfd14b5
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-06 01:10:26 +0000

    changed to add a period when there are zero arguments, please do not be too mad mr.checker

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
-    print("{:d} argument{:s}:".format(args, "s" if args != 1 else ""))
+    print("{:d} argument{:s}{:s}".format(args, "s" if args > 1 else "", "."
+                                         if args <= 1 else ":"))
     for index, arg in enumerate(sys.argv[1:]):
         print("{:d}: {:s}".format(index + 1, arg))
