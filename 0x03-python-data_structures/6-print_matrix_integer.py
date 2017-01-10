329876fdf31c74058576ee0d25c228018e061c1c
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-10 23:01:32 +0000

    completely separating and refactoring

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([' '.join(['{:1d}'.format(item) for item in row])
-                     for row in matrix]))
+    row_array = []
+    for row in matrix:
+        row_array.append(' '.join('{:d}'.format(item) for item in row))
+    print('\n'.join(row_array))
