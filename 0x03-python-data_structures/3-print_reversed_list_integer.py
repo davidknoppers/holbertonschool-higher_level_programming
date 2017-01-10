15b0eaf3cb3ef95511653f14b3fc5943248a81d6
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-10 22:26:37 +0000

    attempting to handling None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(list=[]):
-    for i in reversed(list):
+def print_reversed_list_integer(mylist=[]):
+    if mylist is None:
+        return (None)
+    for i in reversed(mylist):
         print("{:d}".format(i))
-    return
