281fe56f4e7e1acb8077a5e097bd6c338f433836
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-10 22:15:55 +0000

    handling None

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(list, idx, element):
+    if list is None:
+        return (None)
     copy = list[:]
-    if idx > len(copy):
-        return (copy)
+    if idx < 0 or idx > len(copy):
+        return (list)
     copy[idx] = element
     return (copy)
