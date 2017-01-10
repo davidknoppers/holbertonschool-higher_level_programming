c71919e60a15d5163cb422427e29079ca773e377
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-10 22:29:31 +0000

    handling edge case

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(mylist, idx, element):
     if mylist is None:
         return (None)
     copy = mylist[:]
-    if idx < 0 or idx > len(copy):
+    if idx < 0 or idx >= len(copy):
         return (mylist)
     copy[idx] = element
     return (copy)
