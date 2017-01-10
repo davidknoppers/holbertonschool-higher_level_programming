48cdb5af80ce3b60fe59469dfb4cba97a930f7a6
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-10 21:53:23 +0000

    handling empty list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(list, idx, element):
-    if idx < 0 or idx > len(list):
+    if idx < 0 or idx > len(list) or len(list) < 1:
         return(list)
     list[idx] = element
     return(list)
