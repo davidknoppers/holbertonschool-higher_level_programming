48b669194efd111112638dd87b272e4be3286927
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-11 02:37:54 +0000

    tested draft of 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+/**
+ * print_python_list_info - prints size, alloc, and elements of list
+ * @p: pointer to list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, i;
+	Py_ssize_t alloc;
+	PyObject *item;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %zd]\n", alloc);
+	if (PyList_Check(p))
+	{
+		for (i = 0; i < size; i++)
+		{
+			item = PyList_GET_ITEM(p, i);
+			printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+		}
+	}
+}
