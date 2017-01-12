bf70d994f20f72f9cc2dfdcc83a5ca449a52aa5b
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-12 20:42:09 +0000

    removing null checks?

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None:
-        return (None)
-    if my_list == []:
-        return ([])
     for i in range(len(my_list)):
         if my_list[i] == search:
             my_list[i] = replace
