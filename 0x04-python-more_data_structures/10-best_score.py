a68e1e94abee132b0101ebe9d9525b60792d9b0c
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-12 21:24:45 +0000

    changed to return key, not value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if my_dict is None or my_dict == {}:
         return None
     best = None
+    best_key = None
     for key in my_dict.keys():
         if best is None or my_dict[key] > best:
             best = my_dict[key]
-    return best
+            best_key = key
+    return best_key
