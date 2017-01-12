cd6c33e1af556d9a33b8885a993207f525bd0792
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-12 21:46:13 +0000

    first draft of 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None or my_list == []:
+        return 0
+    return (sum(num[0] * num[1] for num in my_list)
+            / sum(num[1] for num in my_list))
