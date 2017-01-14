76034c7ecf27f80d7fd4f12ebb31374e622f6d55
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-14 03:42:34 +0000

    I am not always good with indentation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,8 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result = (a ** b) / i
-                break
-            except:
-                result = b + a
-                break
+        except:
+            result = b + a
+            break
         return result
