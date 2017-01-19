5bb89cdb1ba36d552179dc56f438ee967857d991
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-19 05:56:57 +0000

    changing to text.replace

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -12,18 +12,7 @@ def text_indentation(text):
     """
     if text is None or not isinstance(text, str) or len(text) < 0:
         raise TypeError('text must be a string')
-    result = ""
-    sep = ".?:"
-    sep_flag = 0
-    for char in text:
-        if char in sep:
-            if result[:-1] != " ":
-                result += char
-            result += '\n\n'
-            sep_flag = 1
-        elif sep_flag == 1:
-            sep_flag = 0
-            continue
-        else:
-            result += char
-    print(result)
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    print('\n'.join([line.strip() for line in text.split('\n')]), end="")
