1f4fbba29071733322e202c73d0f96e3d75fb4aa
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-19 06:48:37 +0000

    reordering checks and throwing out group test suite

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -8,26 +8,26 @@ Returns a new matrix with the newly calculated elements
 
 def matrix_divided(matrix, div):
     """
-    Returns a new matrix divided by div
+    matrix_divided returns a  new matrix divided by div
     """
     lol_error = "matrix must be a matrix (list of lists) of integers/floats"
-    if matrix is None or not isinstance(matrix, list):
-        raise TypeError(lol_error)
-    if len(matrix) == 0 or len(matrix[0]) == 0:
-        raise TypeError(lol_error)
-    if not isinstance(div, (int, float)) or div == False or div == True:
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError('div must be a number')
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
-    nested_check = any(not isinstance(i, list) for i in matrix)
-    if nested_check:
+    if matrix == [] or not isinstance(matrix, list):
         raise TypeError(lol_error)
-    row_0 = len(matrix[0])
+    if not all(isinstance(i, list) for i in matrix):
+        raise TypeError(lol_error)
+    if len(matrix[0]) == 0:
+        raise TypeError(lol_error)
     for row in matrix:
-        if row_0 != len(row):
+        if len(row) != len(matrix[0]):
             raise TypeError('Each row of the matrix must have the same size')
-        for elem in row:
-            if not isinstance(elem, (int, float)):
+        for item in row:
+            if isinstance(item, (int, float)):
+                item = int(item)
+            else:
                 raise TypeError(lol_error)
+        if div == 0:
+            raise ZeroDivisionError('division by zero')
     result = [[round(elem / div, 2) for elem in row] for row in matrix]
-    return (result)
+    return result
