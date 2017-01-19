45ba40ec879be8e1a179f295b38b664380232f82
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-19 01:55:32 +0000

    revisions to tests

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -8,7 +8,7 @@ def print_square(size):
     """
     prints a square of hashes
     """
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
