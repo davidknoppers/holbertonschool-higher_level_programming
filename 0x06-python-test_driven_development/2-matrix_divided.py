0d32abdce82b1135e5d011c5969a48b97386df05
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-19 06:17:36 +0000

    risking disaster by uploading small changes

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -13,21 +13,21 @@ def matrix_divided(matrix, div):
     lol_error = "matrix must be a matrix (list of lists) of integers/floats"
     if matrix is None or not isinstance(matrix, list):
         raise TypeError(lol_error)
-    if len(matrix) is 0 or len(matrix[0]) is 0:
+    if len(matrix) == 0 or len(matrix[0]) == 0:
         raise TypeError(lol_error)
-    nested_check = any(not isinstance(i, list) for i in matrix)
-    if nested_check:
-        raise TypeError(lol_error)
-    if type(div) is not int and type(div) is not float:
+    if not isinstance(div, (int, float)) or div == False or div == True:
         raise TypeError('div must be a number')
     if div == 0:
         raise ZeroDivisionError('division by zero')
+    nested_check = any(not isinstance(i, list) for i in matrix)
+    if nested_check:
+        raise TypeError(lol_error)
     row_0 = len(matrix[0])
     for row in matrix:
         if row_0 != len(row):
             raise TypeError('Each row of the matrix must have the same size')
         for elem in row:
-            if not isinstance(elem, int) and not isinstance(elem, float):
+            if not isinstance(elem, (int, float)):
                 raise TypeError(lol_error)
     result = [[round(elem / div, 2) for elem in row] for row in matrix]
     return (result)
