4d4578261d74cd6f23e868e24be7b541c881b78a
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 06:25:01 +0000

    updating to be extra super sure about ints and bools

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -16,11 +16,12 @@ class Square:
         """
         initializes square with a given size or size = 0
         """
-        self.__size = size
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int) or isinstance(size, bool):
             raise TypeError('size must be an integer')
-        if self.__size < 0:
+        if size < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
+
 
     def area(self):
         """
