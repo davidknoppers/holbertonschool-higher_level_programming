43cf7f36ef2d99a4cbbae7e5cdce52150606b887
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 07:42:27 +0000

    added radius dec after checks

--- 0x07-python-classes/103-magic_class.py
+++ 0x07-python-classes/103-magic_class.py
@@ -10,6 +10,7 @@ class MagicClass:
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
+        self.__radius = radius
 
     def area(self):
         """ return area """
