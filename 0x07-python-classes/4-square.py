81e4baeace2ee149deac7ebce9667f82718e89af
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 07:32:59 +0000

    just messing around

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
