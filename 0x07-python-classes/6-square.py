549b608c0d4fa3278e5911b53aa42de6c7840680
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 06:30:51 +0000

    cleaning up error messages and input checks

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-class Square(object):
+"""
+Fancier Square featuring size and position
+Getters and setters implemented for size and position
+Requires a natural number for size
+"""
+
+
+class Square:
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        if self.__size < 0:
+        if size < 0:
             raise ValueError('size must be >= 0')
-        self.__position = position
+        self.__size = size
         if not isinstance(position, tuple) or len(position) != 2:
-            raise TypeError(errormsg)
+            raise TypeError('size must be an integer')
         for i in position:
             if not isinstance(i, int):
-                raise TypeError(errormsg)
+                raise TypeError('size must be an integer')
+        self.__position = position
 
     def area(self):
         return (self.__size ** 2)
