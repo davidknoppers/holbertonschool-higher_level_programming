bac2ba2c83719f7e21f15a5ed45e3742511d8e85
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 07:47:59 +0000

    adding docs

--- 0x07-python-classes/100-singly_linked_list.py
+++ 0x07-python-classes/100-singly_linked_list.py
@@ -7,38 +7,64 @@ Offers basic print function
 
 
 class Node(object):
+    """
+    creates node with next set to None as default
+    """
     def __init__(self, data, next_node=None):
-        self.__data = data
-        if not isinstance(self.__data, int) or isinstance(data, bool):
+        if type(data) is not int or isinstance(data, bool):
             raise TypeError('data must be an integer')
+        self.__data = data
         self.__next_node = next_node
 
     @property
     def data(self):
+        """
+        getter for data
+        """
         return (self.__data)
 
     @data.setter
     def data(self, value):
+        """
+        setter for data
+        """
         if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
+        """
+        getter for node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if value is not None and not isinstance(value, Node):
+        """
+        setter for node
+        """
+        if value is not None and type(value) is not Node:
             raise TypeError('next must be a Node object')
         self.__next_node = value
 
 
 class SinglyLinkedList(object):
+    """
+    Singly linked list
+    Uses node class
+    Contains a print function and sorts by int value
+    """
     def __init__(self):
+        """
+        init
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        inserts by int value
+        """
         if not self.__head:
             self.__head = Node(value, None)
         else:
@@ -55,6 +81,9 @@ class SinglyLinkedList(object):
                 prev.next_node = Node(value, current)
 
     def __repr__(self):
+        """
+        formats list for output
+        """
         result = ""
         temp = self.__head
         while temp:
