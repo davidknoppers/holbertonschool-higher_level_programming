413fe5c0e01c99ac3b8501e24450d095981fed9a
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 06:18:11 +0000

    removing dis and unnecessary imports

--- 0x07-python-classes/103-magic_class.py
+++ 0x07-python-classes/103-magic_class.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import dis
 import math
-class MagicClass(object):
+
+
+class MagicClass:
+
     def __init__(self, radius):
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = 0
 
     def area(self):
-        from math import pi
-        return (self.__radius ** 2 * math.pi)
+        return ((self.__radius ** 2) * math.pi)
 
     def circumference(self):
-        return (self.__radius * 2 * math.pi)
-print(dis.dis(MagicClass))
+        return (2 * math.pi * self.__radius)
