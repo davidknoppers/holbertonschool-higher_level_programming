eeaef25afbf8cc63e586e34deaf8e66b21c4d1b8
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-24 06:39:06 +0000

    adding docs

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -8,18 +8,30 @@ Requires a natural number for size
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
+        """
+        initialize square with size and pos
+        """
         self.__size = size
         self.__position = position
 
     def area(self):
+        """
+        return area by squaring size
+        """
         return (self.__size ** 2)
 
     @property
     def size(self):
+        """
+        getter for size
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """
+        setter for size
+        """
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if self.__size < 0:
@@ -28,10 +40,16 @@ class Square:
 
     @property
     def position(self):
+        """
+        getter for pos
+        """
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """
+        setter for pos
+        """
         errormsg = 'position must be a tuple of 2 positive integers'
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError(errormsg)
@@ -41,6 +59,9 @@ class Square:
         self.__position = value
 
     def my_print(self):
+        """
+        wild print function
+        """
         print("\n" * self.__position[1], end="")
         print("\n".join([" " * self.position[0] + "#" * self.__size
                          for i in range(self.__size)]))
