52a39561b62cdc0cb06258ac195fd5213f381d5d
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-01 17:05:09 +0000

    incorporating __new__

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,6 +6,12 @@ MyInt challenges us to change the basics of an int
 
 
 class MyInt(int):
+    def __new__(cls, value):
+        """
+        create a new instance of class cls
+        """
+        return super().__new__(cls, value)
+
     def __eq__(self, other):
         """
         !=
