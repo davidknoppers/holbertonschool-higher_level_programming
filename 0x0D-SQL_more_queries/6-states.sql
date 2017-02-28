371dcab6f17f729d732674991d6c42108f4f9dd7
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-28 21:26:19 +0000

    SQL queries can be very literal

---
+++ 0x0D-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates hbtn_0d_usa database and states table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+name VARCHAR(256) NOT NULL
+);
