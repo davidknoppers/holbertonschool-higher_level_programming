a9fc571d0faad9893b2121016a8e9d9bf5bcfd83
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-01 00:11:49 +0000

    including rename

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_show_genres.name, COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_show_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
