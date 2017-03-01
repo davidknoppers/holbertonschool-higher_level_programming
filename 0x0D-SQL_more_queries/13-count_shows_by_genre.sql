57a0c8cfdbbc3b5575535a05b333219c9bab38cb
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-01 00:15:49 +0000

    trying to reverse typo in 13

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
--- lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_show_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_shows
+-- show all genres and how many links they have
+
+SELECT tv_show_genres.name as 'genre', COUNT(tv_show_genres.genre_id) AS 'number_shows'
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
