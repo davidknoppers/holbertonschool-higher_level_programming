d43c0e57f5be4b7bdbf17382ab4ca6e1be09b591
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-09 05:47:39 +0000

    missed all checks, is that bad

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# prints all states
+"""
+prints all states
+"""
 if __name__ == "__main__":
     import sys
     import MySQLdb
 
-    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
 
-    query = ("SELECT id, name FROM states ORDER BY id asc;")
+    query = ("SELECT id, name FROM states ORDER BY states.id asc;")
 
     cursor.execute(query)
 
