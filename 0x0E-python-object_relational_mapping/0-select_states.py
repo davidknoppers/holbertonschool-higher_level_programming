4c33d3d206bb0c988f6110fcf3fbd96b0cd02531
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-09 05:49:39 +0000

    moved imports out of name main check

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,16 @@
 """
 prints all states
 """
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
 
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
 
-    query = ("SELECT id, name FROM states ORDER BY states.id asc;")
+    query = ("SELECT id, name FROM states ORDER BY states.id asc")
 
     cursor.execute(query)
 
