47ccb8aadc492eaa37b3adcc47bb9e2130aacfb4
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-09 18:23:23 +0000

    name main protection

--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-comment
+prints some cities and states
 """
 import sys
 import MySQLdb
 
-db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
-cursor = db.cursor()
+if __name__ == "__main__":
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
 
 
-query = """SELECT cities.id, cities.name, states.name FROM cities
-JOIN states on cities.state_id = states.id
-ORDER BY cities.id ASC;"""
-cursor.execute(query)
+    query = """SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states on cities.state_id = states.id
+    ORDER BY cities.id ASC;"""
+    cursor.execute(query)
 
-for row in cursor.fetchall():
-    print(row)
+    for row in cursor.fetchall():
+        print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
