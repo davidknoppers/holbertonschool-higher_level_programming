ebd193c2cf22ef3497d4c23e6fd5695132a9f1df
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-09 21:16:52 +0000

    lightly tested draft

---
+++ 0x0E-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Deletes any state that contains an a
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                         sys.argv[2],
+                                                         sys.argv[3])
+    db = create_engine(engine)
+    Base.metadata.create_all(db)
+    session1 = sessionmaker(bind=db)
+    session2 = session1()
+
+    for instance in session2.query(State).order_by(State.id).filter(
+            State.name.contains('a')):
+        session2.delete(instance)
