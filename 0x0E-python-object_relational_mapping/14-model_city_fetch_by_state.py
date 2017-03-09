4d01b7b2554da904b7a7b2ad64c3b7f50b982f84
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-09 21:42:51 +0000

    untested drafts, will discuss with Daniel

---
+++ 0x0E-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Like number 7 for cities
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import cities
+if __name__ == "__main__":
+    engine = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                         sys.argv[2],
+                                                         sys.argv[3])
+    db = create_engine(engine)
+    Base.metadata.create_all(db)
+    session1 = sessionmaker(bind=db)
+    session2 = session1()
+
+    for instance in session2.query(State.name, City.id, City.name).filter(
+            State.id == City.state.id).order_by(City,id):
+        print("{}: {{}} {}".format(instance[0], instance[1], instance[2])
+    session2.commit()
+    session2.close()
+    db.dispose()
---
+++ 0x0E-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Connecting a class to MySQL"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+Base = declarative_base()
+
+
+class City(Base):
+    """Basic city class """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
