25a9c8c4335a298cdad96dbb2b0dc3fd9d48ee49
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-09 19:01:08 +0000

    is it the quotes?

--- 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     session1 = sessionmaker(bind=engine)
