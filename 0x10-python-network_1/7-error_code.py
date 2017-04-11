a37004a9e83beb78f67a1f6e1bc70fed6b608468
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-11 20:51:14 +0000

    first real draft of 7

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -6,10 +6,10 @@ the value of the X-Request-Id variable found in the header of the response
 import requests
 import sys
 
-
-url = sys.argv[1]
-r = requests.get(url)
-if r.status_code and int(r.status_code) >= 400:
-    print("Error code: {}".format(r.status_code))
-else:
-    print(r.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code and int(r.status_code) >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
