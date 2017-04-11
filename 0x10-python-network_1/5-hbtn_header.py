36763b8f11ee36503dacdc5a905f812cdc3762a6
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-11 20:33:36 +0000

    had accidentally hardcoded a url; rewritten to take url as arg

--- 0x10-python-network_1/5-hbtn_header.py
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -4,8 +4,10 @@ Takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response
 """
 import requests
+import sys
 
 
+url = sys.argv[1]
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(url)
     print(r.headers['X-Request-Id'])
