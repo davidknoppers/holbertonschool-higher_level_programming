54c789a0f625732b377a7c4bc7bb1510ecfe3c99
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-11 02:58:05 +0000

    fixing comment per previous two commits

--- 0x0F-python-network_0/38-delete.sh
+++ 0x0F-python-network_0/38-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL as a variable, sends a request to that URL,
-# sends a delete request to the URL, and displays the response
+# takes in a URL as a variable, sends a request to that URL, sends a delete request to the URL, and displays the response
 curl -LsX DELETE "$1"
