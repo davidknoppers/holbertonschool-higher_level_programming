5b8c8961580fe21dd4c4ab5af1a175a929182781
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-11 02:56:36 +0000

    changing comment from two lines to one

--- 0x0F-python-network_0/37-body.sh
+++ 0x0F-python-network_0/37-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL as a variable, sends a request to that URL,
-# and displays only the body of a 200 status code response
-curl -Lsf -X GET "$1"
+# takes in a URL as a variable, sends a request to that URL, and displays only the body of a 200 status code response
+curl -LsfX GET "$1"
