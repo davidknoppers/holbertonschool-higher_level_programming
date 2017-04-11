f16397208f157b9bfecf39013779309d9c00744b
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-11 20:45:00 +0000

    putting print statement in try/except

--- 0x10-python-network_1/5-hbtn_header.py
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -10,5 +10,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-    if r.headers['X-Request-Id']:
-        print(r.headers['X-Request-Id'])
+    try:
+        temp = r.headers['X-Request-Id']
+        print(temp)
+    except:
+        pass
