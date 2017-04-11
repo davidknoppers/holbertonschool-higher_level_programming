5e04c45a966a259c4fe060cbfd3f0e9b2cc87891
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-11 20:11:38 +0000

    trying to fix formatting

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -8,11 +8,11 @@ import urllib.parse
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('ascii')
-with urllib.request.urlopen(url, data) as response:
-    email_address = (response.read()).decode('utf-8')
-    print(email_address)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('ascii')
+    with urllib.request.urlopen(url, data) as response:
+        email_address = (response.read()).decode('utf-8')
+        print(email_address)
