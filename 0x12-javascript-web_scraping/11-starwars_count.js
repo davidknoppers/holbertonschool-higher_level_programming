d238dc771cb1b7b8ecdf9b035851af924f5cc00b
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-30 23:09:35 +0000

    draft of 11, error handling likely not proper

---
+++ 0x12-javascript-web_scraping/11-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+var request = require('request');
+var options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  let count = 0;
+  for (const e of JSON.parse(body)['results']) {
+    for (const subE of e['characters']) {
+      if (subE.includes('18')) {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
