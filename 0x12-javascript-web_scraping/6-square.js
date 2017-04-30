f6cd2e3dace341dd75448e5acae6531e085500ca
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-30 21:53:16 +0000

    maybe working version of 6

---
+++ 0x12-javascript-web_scraping/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Square = require('./5-square').Square;
+
+Square.prototype.charPrint = function (c) {
+  var array = [];
+  if (c === undefined) {
+    array = Array(this.width).fill(Array(this.height + 1).join('X'));
+  } else {
+    array = Array(this.width).fill(Array(this.height + 1).join(c));
+  }
+  for (let s of array) {
+    console.log(s);
+  }
+};
+
+exports.Square = Square;
